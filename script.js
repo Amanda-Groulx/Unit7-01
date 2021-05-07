2387b7a3a3deb20a00024e864de25a33ca7bfbfa
AmandaGroulx <>
2021-05-07 14:52:08 +0000

    added spaces and changed randomNUMBER to const.

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-//Connect "Guess" button to "random function"
+//  Connect "Guess" button to "random function"
 document.getElementById('button').addEventListener('click', randomizer)
 let randomNumber = 0
 let UserGuess = 0
 
 function randomizer () {
-  //get number typed in by user and convert to an integer
-  userGuess = document.getElementById('input').value
-  userGuess = parseInt(userGuess)
+  //  get number typed in by user and convert to an integer
+  const = document.getElementById('input').value
+  const = parseInt(userGuess)
 
-  //generate random number between 1 and 6
+  //  generate random number between 1 and 6
   randomNumber = (Math.random() * 6) + 1
   randomNumber = parseInt(randomNumber)
 
-  //compare user's guess with random number
-  if (UserGuess == randomNumber) {
-    document.getElementById('answer').innerHTML = "You win!"
+  //  compare user's guess with random number
+  if (UserGuess === randomNumber) {
+    document.getElementById('answer').innerHTML = 'You win!'
   }
 }
